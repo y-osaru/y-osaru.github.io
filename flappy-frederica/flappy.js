aa386761e171fe84d573d17436bb94dccbf91fef
y-osaru <osaru.tus.1410049@gmail.com>
2019-02-09 14:13:54 +0900

    flappy fredericaにtwitter card追加&ゲーム部分実装修正

--- flappy-frederica/flappy.js
+++ flappy-frederica/flappy.js
@@ -3,19 +3,21 @@
  */
 phina.globalize();
 
-var ASSETS = {
+let ASSETS = {
   image:{
     bg:"assets/bg2.png",
     fre:"assets/fre.png",
     ueki1:"assets/ob1.png",
-    ueki2:"assets/ob2.png"
+    ueki2:"assets/ob2.png",
+    share:"assets/Twitter.png",
+    restart:"assets/restart.png"
   }
 };
 
-var SPEED = 1;
-var SCREEN_WIDTH = 300;  
-var SCREEN_HEIGHT = 465;
-var BG_WIDTH = 321;
+let SPEED = 1;
+let SCREEN_WIDTH = 300;  
+let SCREEN_HEIGHT = 465;
+let BG_WIDTH = 321;
 
 
 //背景
@@ -27,7 +29,7 @@ phina.define('BackGround',{
     this.superInit();
     
     //背景を無限スクロールする為に2枚をつなげる
-    var bg = Sprite("bg").addChildTo(this);
+    let bg = Sprite("bg").addChildTo(this);
     bg.setOrigin(0,0); 
     bg.update = function(){
       bg.x -= SPEED;
@@ -36,7 +38,7 @@ phina.define('BackGround',{
       }
     }
     
-    var bg2 = Sprite("bg").addChildTo(this);
+    let bg2 = Sprite("bg").addChildTo(this);
     bg2.setOrigin(0,0);
     bg2.x = BG_WIDTH;//初期位置を幅分ずらす
     bg2.update = function(){
@@ -48,11 +50,29 @@ phina.define('BackGround',{
   }
 });
 
+//スコア判定用Shape
+let scoreCol;
+phina.define('ScoreCol',{
+  superClass:'DisplayElement',
+  init:function(){
+    this.superInit();
+
+    scoreCol = Shape({
+      width: 23,
+      height: SCREEN_HEIGHT,
+    }).addChildTo(this);
+    scoreCol.setOrigin(0,0);
+    scoreCol.alpha = 0;
+  }
+});
+
 //TODO:ココらへんは配列にした方が良さげ
 let ob1;
 let ob2;
 let ob3;
 let ob4;
+const RAND_MIN = 50;
+const RAND_MAX = 135
 phina.define('Obstacle',{
   superClass:'DisplayElement',
   
@@ -63,7 +83,7 @@ phina.define('Obstacle',{
     ob1.width = 25;
     ob1.height = 400;
     ob1.setOrigin(0,0);
-    var rand = Math.randint(0,250);
+    let rand = Math.randint(RAND_MIN,RAND_MAX);
     ob1.setPosition(200,rand - ob1.height);
     
     ob2 = Sprite("ueki2").addChildTo(this);
@@ -72,7 +92,7 @@ phina.define('Obstacle',{
     
     ob3 =Sprite("ueki1").addChildTo(this);
     ob3.setOrigin(0,0);
-    rand = Math.randint(0,250);
+    rand = Math.randint(RAND_MIN,RAND_MAX);
     ob3.setPosition(425,rand - ob3.height);
     
     ob4 = Sprite("ueki2").addChildTo(this);
@@ -84,23 +104,24 @@ phina.define('Obstacle',{
     ob1.x -= SPEED * 3;
     ob2.x = ob1.x;
     if(ob1.x <= -1 * ob1.width){
-      var rand = Math.randint(0,250);
+      let rand = Math.randint(RAND_MIN,RAND_MAX);
       ob1.setPosition(425,rand - ob1.height)
       ob2.setPosition(425,rand + 150);
+      ob1.hitted = false;
     }
     
     ob3.x -= SPEED * 3;
     ob4.x = ob3.x;
     if(ob3.x <= -1 * ob3.width){
-      rand = Math.randint(0,250);
+      rand = Math.randint(RAND_MIN,RAND_MAX);
       ob3.setPosition(425,rand - ob3.height)
       ob4.setPosition(425,rand + 150);
+      ob3.hitted = false;
     }
   }
 });
 
 let player;
-let lab;
 phina.define('Player',{
   superClass:'DisplayElement',
   init:function(){
@@ -121,7 +142,7 @@ phina.define('TitleScene',{
   init:function(){
     this.superInit();
     //背景画像
-    var bg = Sprite("bg").addChildTo(this);
+    let bg = Sprite("bg").addChildTo(this);
     bg.setOrigin(0,0); 
     
     //キャラ
@@ -135,19 +156,17 @@ phina.define('TitleScene',{
     ob1.width = 25;
     ob1.height = 400;
     ob1.setOrigin(0,0);
-    var rand = Math.randint(0,250);
-    ob1.setPosition(200,rand - ob1.height);
+    ob1.setPosition(200,-250);
     
     ob2 = Sprite("ueki2").addChildTo(this);
     ob2.setOrigin(0,0);
-    ob2.setPosition(200,rand + 150);
+    ob2.setPosition(200,350);
     
-    var label = Label(
+    Label(
       {
         text:'Flappy Frederica',
         fontSize: 20,
-      }).addChildTo(this);
-    label.setPosition(150,200)
+      }).addChildTo(this).setPosition(150,200);
   },
   onclick:function(){
     this.exit();
@@ -156,7 +175,11 @@ phina.define('TitleScene',{
 
 //メインシーン
 let score = 0;
+let rank = "実装中...";
 const SCORE_PREFIX = "Score:";
+const RANK_PREFIX = "Rank:";
+const RANK_LOADING = "ランキング取得中...";
+let scoreLab;
 phina.define('MainScene', {
   superClass: 'DisplayScene',
   
@@ -166,16 +189,18 @@ phina.define('MainScene', {
     BackGround().addChildTo(this);
     Obstacle().addChildTo(this);
     Player().addChildTo(this);
+    ScoreCol().addChildTo(this);
     
     this.onpointstart = function(){
       player.physical.velocity.y = -10;
     };
     
-    lab = Label({
+    scoreLab = Label({
       text: SCORE_PREFIX + score,
       fontSize: 20,
       fill: 'red',
-    }).addChildTo(this).setPosition(40,10);
+      align:'left'
+    }).addChildTo(this).setPosition(0,10);
     
     score = 0
   },
@@ -189,16 +214,17 @@ phina.define('MainScene', {
       this.exit();
     }
     
-    if(ob1.x === 20){
-      ///フレーム抜けとかありそう。ギリギリに判定用の透明オブジェクト必要かも
+    if(ob1.hitTestElement(scoreCol) && !ob1.hitted){
       score++;
+      ob1.hitted = true;
     }
-    if(ob3.x === 20){
-      ///フレーム抜けとかありそう。ギリギリに判定用の透明オブジェクト必要かも
+
+    if(ob3.hitTestElement(scoreCol) && !ob3.hitted){
       score++;
+      ob3.hitted = true;
     }
-    
-    lab.text = SCORE_PREFIX + score;
+
+    scoreLab.text = SCORE_PREFIX + score;
   }
 });
 
@@ -211,22 +237,51 @@ phina.define('ResultScene',{
     Label({
       text: SCORE_PREFIX + score,
       fontSize: 20,
-    }).addChildTo(this).setPosition(150,100);
+      align:'left'
+    }).addChildTo(this).setPosition(100,100);
     
-    var fre = Sprite("fre").addChildTo(this);
-    fre.setOrigin(0,0);
-    fre.setPosition(80,150);
+    Label({
+      text: RANK_PREFIX + "実装中...",
+      fontSize: 20,
+      align:'left'
+    }).addChildTo(this).setPosition(100,130);
 
-    var dokan = Sprite("ueki2").addChildTo(this);
-    dokan.setOrigin(0,0);
-    dokan.setPosition(50,300);
-    dokan.width = 200;   
-    dokan.height = 3200;
+    let fre = Sprite("fre").addChildTo(this);
+    fre.width = 63;
+    fre.height = 106;
+    fre.setOrigin(0,0);
+    fre.setPosition(120,150);
+
+    let share = Sprite("share").addChildTo(this);
+    share.width = 64;
+    share.height = 64;
+    share.setOrigin(0,0);
+    share.setPosition(50,300);
+    share.setInteractive(true);
+    share.onpointstart = function(){
+      let url = phina.social.Twitter.createURL({
+        text: 'Flappy Fredericaで遊んだよ！\r\nScore:'+score+'\r\nRank:'+rank+'\r\n',
+        hashtags: 'imag_cg,宮本フレデリカ,ふらフレ',
+        url: 'https://y-osaru.github.io/flappy-frederica/flappy.html'
+      });
+      window.location.href = url;
+    };
+
+    let self = this;
+    let restart = Sprite("restart").addChildTo(this);
+    restart.width = 64;
+    restart.height = 64;
+    restart.setOrigin(0,0);
+    restart.setPosition(186,300);
+    restart.setInteractive(true);
+    restart.onpointstart = function(){
+      self.exit();
+    }
 
     this.backgroundColor = "#ff69b4";
   },
   onclick: function(){
-    this.exit();
+    //this.exit();
   }
 });
 
@@ -249,7 +304,7 @@ const myScenes =  [
 ];
 
 phina.main(function() {
-  var app = GameApp({
+  let app = GameApp({
     startLabel: 'title',
     width: SCREEN_WIDTH,
     height: SCREEN_HEIGHT,
